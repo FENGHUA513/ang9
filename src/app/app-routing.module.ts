69030fa4f22a514ca54afca5a26d3a5a71b7bed7
xiong <597709174@qq.com>
2020-03-21 21:29:53 +0800

    update material module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { MainComponent } from './pages/main/main.component';
+const routes: Routes = [
+  { path: 'main',
+    component: MainComponent
+  },
+  { path: '', redirectTo: 'main', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
+  exports: [RouterModule],
+  // providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppRoutingModule { }
