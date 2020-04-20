4223079292421a0ac464f39538f7f01f9badbe63
xiong <597709174@qq.com>
2020-04-20 15:04:23 +0800

    add i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core'
-
+import { TranslateService } from '@ngx-translate/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular9'
+  constructor(private translate: TranslateService) {}
+  private lang: string = 'zh'
+  ngOnInit() {
+    // ngx-translate国际化服务相关的配置
+    this.translate.addLangs(['zh', 'en'])
+    this.translate.setDefaultLang(this.lang)
+    const browserLang = this.translate.getBrowserLang()
+    console.log('获取到浏览器的语言 = ' + browserLang)
+    this.translate.use(browserLang.match(/zh|en/) ? browserLang : 'en')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { SharedModule } from './shared/shared.module'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MainComponent } from './pages/main/main.component';
+import { MainComponent } from './pages/main/main.component'
 import { HeaderComponent } from './components/header/header.component'
-
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http)
+}
 @NgModule({
   declarations: [AppComponent, MainComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
